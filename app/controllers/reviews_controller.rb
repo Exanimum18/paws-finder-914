d1f9c271afce2da392fa015496da1fcc857ebbab
Santiago Baron Castaño <santiago.baron.18@gmail.com>
2022-10-17 16:38:28 -0500

    Controlador de reviews casi terminado

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,36 @@
 class ReviewsController < ApplicationController
+
   def create
+    @review = Review.new(review_params)
+    @post = Post.find(params[:post_id])
+    @review.post = @post
+    if @review.save
+      redirect_to post_path(@post)
+    else
+      render "posts/show", status: :unprocessable_entity
+    end
   end
 
   def edit
+    set_review
   end
 
   def update
   end
 
   def destroy
+    set_review
+    @review.destroy
+    redirect_to post_path(@review.post)
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
