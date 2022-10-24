10c87f5bd720208b18a4a740818a4b3c569fe2c0
Santiago Baron Castaño <santiago.baron.18@gmail.com>
2022-10-24 13:04:22 -0500

    El search no funcionaba por que estaba comentado en el modelo de post

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,11 +24,11 @@ class Post < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  #include PgSearch::Model
+  include PgSearch::Model
 
-  #pg_search_scope :search_by_description_and_address_and_title,
-  #against: [ :description, :address, :title],
-  #using: {
-  # tsearch: { prefix: true }
-  #}
+  pg_search_scope :search_by_description_and_address_and_title,
+  against: [ :description, :address, :title],
+  using: {
+  tsearch: { prefix: true }
+  }
 end
